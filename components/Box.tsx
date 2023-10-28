162507c3ae9876ea5b77c18ae1a42dbc394be9c1
Sai Rohit <sairohitp007@gmail.com>
2023-10-29 00:11:59 +0530

    sidebar progress

--- components/Box.tsx
+++ components/Box.tsx
@@ -1,5 +1,17 @@
-const Box = () => {
-    return (  );
+interface BoxProps {
+    children: React.ReactNode;
+    className?: string;
+}
+
+const Box: React.FC<BoxProps> = ({
+    children,
+    className
+}) => {
+    return ( 
+        <div>
+            {children}
+        </div>
+     );
 }
  
 export default Box;
