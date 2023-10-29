0b4e22134ac28999b1e0f5c65deaae4246f47057
Sai Rohit <sairohitp007@gmail.com>
2023-10-29 16:18:02 +0530

    library component concludes

--- components/Library.tsx
+++ components/Library.tsx
@@ -31,7 +31,25 @@ const Library = () => {
                         Your Library
                     </p>
                 </div>
-                <AiOutlinePlus/>
+                <AiOutlinePlus
+                    onClick = {onClick}
+                    size = {20}
+                    className="
+                        text-neutral-400
+                        cursor-pointer
+                        hover:text-white
+                        transition
+                    "
+                />
+            </div>
+            <div className="
+                flex
+                flex-col
+                gap-y-2
+                mt-4
+                px-3
+            ">
+                List of Songs
             </div>
         </div>
      );
