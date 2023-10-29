d4099fd3d7121a6313cf3fb85cb60eeaba578abf
Sai Rohit <sairohitp007@gmail.com>
2023-10-29 16:10:41 +0530

    Library component progress

--- components/Library.tsx
+++ components/Library.tsx
@@ -1,9 +1,38 @@
 "use client";
 
+import { TbPlaylist } from "react-icons/tb"
+import { AiOutlinePlus } from "react-icons/ai"
+
 const Library = () => {
+    const onClick = () => {
+        // handle upload later
+    }
+
     return ( 
-        <div>
-            Library
+        <div className="flex flex-col">
+            <div className="
+                flex
+                items-center
+                justify-between
+                px-5
+                pt-4
+            ">
+                <div className="
+                    inline-flex
+                    items-center
+                    gap-x-2
+                ">
+                    <TbPlaylist className = "text-neutral-400" size = {26} />
+                    <p className="
+                        text-neutral-400
+                        font-medium
+                        text-md
+                    ">
+                        Your Library
+                    </p>
+                </div>
+                <AiOutlinePlus/>
+            </div>
         </div>
      );
 }
