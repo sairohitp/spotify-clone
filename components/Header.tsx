7fd93e5a8747b61ce23a76caf69e1120d2b67732
Sai Rohit <sairohitp007@gmail.com>
2023-11-02 19:51:54 +0530

    md:hi-icon

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,6 +4,7 @@ import { useRouter } from "next/navigation";
 import { twMerge } from "tailwind-merge";
 
 import {RxCaretLeft, RxCaretRight} from "react-icons/rx"
+import {HiHome} from "react-icons/hi"
 
 interface HeaderProps {
     children: React.ReactNode
@@ -20,7 +21,7 @@ const Header: React.FC<HeaderProps> = ({
         // Handle logout
     }
 
-    return ( 
+    return (
         <div className={twMerge(`
             h-fit
             bg-gradient-to-b
@@ -42,7 +43,9 @@ const Header: React.FC<HeaderProps> = ({
                     gap-x-2
                     items-center
                 ">
-                    <button className="
+                    <button 
+                    onClick={() => router.back()}
+                    className="
                         rounded-full
                         bg-black
                         flex
@@ -53,7 +56,9 @@ const Header: React.FC<HeaderProps> = ({
                     ">
                         <RxCaretLeft className = "text-white" size = {35}/>
                     </button>
-                    <button className="
+                    <button 
+                    onClick={() => router.forward()}
+                    className="
                         rounded-full
                         bg-black
                         flex
@@ -63,6 +68,20 @@ const Header: React.FC<HeaderProps> = ({
                         transition
                     ">
                         <RxCaretRight className = "text-white" size = {35}/>
+                    </button>
+                </div>
+                <div className="flex md:hidden gap-x-2 items-center">
+                    <button className="
+                        rounded-full
+                        p-2
+                        bg-white
+                        flex
+                        items-center
+                        justify-center
+                        hover:opacity-75
+                        transition
+                    ">
+                        <HiHome className = "text-black" size = {20}/>
                     </button>
                 </div>
 
