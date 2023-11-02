fd5ad9b61c921ca7454a21487e4822234906a4ca
Sai Rohit <sairohitp007@gmail.com>
2023-11-02 22:30:49 +0530

    ListItem

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -1,4 +1,19 @@
-const ListItem = () => {
+"use client";
+
+import { useRouter } from "next/navigation";
+
+interface ListItemProps {
+    image: string;
+    name: string;
+    href: string;
+}
+
+const ListItem: React.FC<ListItemProps> = ({
+    image,
+    name,
+    href
+}) => {
+    const router = useRouter
     return ( 
         <div>
             List Item
