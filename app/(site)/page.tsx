6886e83a97d72583b29bb81885b3870467fd933f
Sai Rohit <sairohitp007@gmail.com>
2023-11-03 22:04:04 +0530

    server

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -38,25 +38,12 @@ export default function Home() {
           </div>
         </div>
       </Header>
-      <div className="
-        mt-2
-        mb-7
-        px-6
-      ">
-        <div className="
-          flex
-          justify-between
-          items-center
-        ">
-          <h1 className="
-            text-white
-            text-2xl
-            font-semibold
-          ">Newest Songs
+      <div className="mt-2 mb-7 px-6">
+        <div className="flex justify-between items-center">
+          <h1 className="text-white text-2xl font-semibold">
+            Newest songs
           </h1>
-
         </div>
-
       </div>
     </div>
   )
