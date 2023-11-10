e383cb9e0b9566d9c0ef4d557bb2425c67bcf048
Sai Rohit <sairohitp007@gmail.com>
2023-11-10 08:50:19 +0530

    user table attributes furnished

--- database.sql
+++ database.sql
@@ -4,6 +4,13 @@
 */
 
 create table users {
-
+    --  UUID from auth.users
+    id uuid references auth.users not null primary key,
+    full_name text,
+    avatar_url text,
+    -- The user's billing address stored in JSON format
+    billing_address jsonb,
+    -- Stores your cutomer's payment instruments
+    payment_method jsonb
 };
 
