38c3ddafa7ac4c3a92392992d42ff42a53d774a2
Sai Rohit <sairohitp007@gmail.com>
2023-11-11 22:39:55 +0530

    public function

--- database.sql
+++ database.sql
@@ -37,3 +37,9 @@ $$
     end
 $$
 
+language plpgsql security definer;
+
+create trigger on_auth_user_created
+  after insert on auth.users
+  for each row
+    execute procedure public.handle_new_user();
