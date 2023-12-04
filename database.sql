606e531a8dc32b1cf5108319445f4c80297a97c0
Sai Rohit <sairohitp007@gmail.com>
2023-12-04 23:57:29 +0530

    db

--- database.sql
+++ database.sql
@@ -1,45 +1,52 @@
-/*
+/**
 * USERS
-* Template used for the table containing user data. Users should only be able to view and update their own data.
+* Note: This table contains user data. Users should only be able to view and update their own data.
 */
-
-create table users {
-    --  UUID from auth.users
-    id uuid references auth.users not null primary key,
-    full_name text,
-    avatar_url text,
-    -- The user's billing address stored in JSON format
-    billing_address jsonb,
-    -- Stores your cutomer's payment instruments
-    payment_method jsonb
-};
-
+create table users (
+  -- UUID from auth.users
+  id uuid references auth.users not null primary key,
+  full_name text,
+  avatar_url text,
+  -- The customer's billing address, stored in JSON format.
+  billing_address jsonb,
+  -- Stores your customer's payment instruments.
+  payment_method jsonb
+);
 alter table users
-    enable row level security;
-
+  enable row level security;
 create policy "Can view own user data." on users
-    for select using (auth.uid() = id);
-
+  for select using (auth.uid() = id);
 create policy "Can update own user data." on users
-    for update using (auth.uid() = id);
+  for update using (auth.uid() = id);
 
-/** 
-* This trigger automatically creates a user entry when a new user signs up via Supabase Auth
+/**
+* This trigger automatically creates a user entry when a new user signs up via Supabase Auth.
 */
-
 create function public.handle_new_user()
 returns trigger as
 $$
-    begin
-        insert into public.users (id, full_name, avatar_url)
-        values (new.id, new.raw_user_meta_data ->> "full_name", new.raw_user_meta_data ->> "avatar_url");
-        return new;
-    end
+  begin
+    insert into public.users (id, full_name, avatar_url)
+    values (new.id, new.raw_user_meta_data->>'full_name', new.raw_user_meta_data->>'avatar_url');
+    return new;
+  end;
 $$
-
 language plpgsql security definer;
 
 create trigger on_auth_user_created
   after insert on auth.users
   for each row
     execute procedure public.handle_new_user();
+
+/**
+* CUSTOMERS
+* Note: this is a private table that contains a mapping of user IDs to Strip customer IDs.
+*/
+create table customers (
+  -- UUID from auth.users
+  id uuid references auth.users not null primary key,
+  -- The user's customer ID in Stripe. User must not be able to update this.
+  stripe_customer_id text
+);
+alter table customers enable row level security;
+-- No policies as this is a private table that the user must not have access to.
